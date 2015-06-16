d5816fcf809f73edd37bdaa6443544e5461762af
satheeshraj <satheeshraj@ymail.com>
2015-06-16 18:00:23 +0530

    first commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,37 @@
 
 var spreeMarketController = angular.module('spreeMarketController', []);
 
-spreeMarket.controller('MainCtrl',['$scope',function($scope){
+spreeMarket.controller('MainCtrl',['$scope','$log',function($scope,$log){
 
+$(".links").mouseover(function(){
+    $(this).css({'border-bottom': '2px solid'})
+}).mouseout(function(){
+    $(this).css({'border-bottom': 'none'})
+});
 
+$("#button").mouseover(function(){
+    $(this).css({'background-color': 'white','color':'black !important'})
+}).mouseout(function(){
+    $(this).css({'background-color': 'black','color':'white !important'})
+});
+
+    //$('html, body').animate({scrollTop:0 }, function(){
+    //    $log.debug("33333");
+    //});
+
+   $(window).scroll(function(){
+       var window_top = $(window).scrollTop();
+      if(window_top != 0){
+          $('#nav').css({'background-color': 'black','border-color':'black'})
+       }else{
+          $(nav).scrollTop(function(){
+              $(this).css({'background-color': 'transparent','border-color':'transparent'})
+          })
+
+       //   $(nav).scroll(function(){
+       //       $(this).css({'background-color': 'transparent','border-color':'transparent'})
+       //   })
+      }
+   })
 
 }]);
